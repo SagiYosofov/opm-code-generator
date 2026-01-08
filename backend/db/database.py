f054bbad3fa8bac0217ec141068cea0c91285b64
SagiYosofov <Sagi.Yosofov@e.braude.ac.il>
2026-01-08 18:59:13 +0200

    add opm_generations collection

--- backend/db/database.py
+++ backend/db/database.py
@@ -7,7 +7,9 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("MONGO_DB_NAME")
 USERS_COLLECTION_NAME = "users"
+OPM_GENERATIONS_COLLECTION_NAME = "opm_generations"
 
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
 users_collection = db[USERS_COLLECTION_NAME]
+opm_generations_collection = db[OPM_GENERATIONS_COLLECTION_NAME]
