b2f6a0c3c15c14995535305b22d870daa14c9628
SagiYosofov <Sagi.Yosofov@e.braude.ac.il>
2026-01-17 22:02:53 +0200

    update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { UserProvider } from "./context/UserContext";
+import { ThemeProvider } from "./context/ThemeContext";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UserProvider>
+    <ThemeProvider>
+      <UserProvider>
         <App />
-    </UserProvider>
+      </UserProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
